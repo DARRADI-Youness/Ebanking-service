d35220c01ca40061f46e35cf33f324814b336a4b
Darradi Youness <darradi.youness@outlook.com>
2022-11-01 11:13:42 +0100

    Interface P1

--- src/main/java/org/sid/ebankingservice/repositories/BankAccountRepository.java
+++ src/main/java/org/sid/ebankingservice/repositories/BankAccountRepository.java
@@ -1,8 +1,14 @@
 package org.sid.ebankingservice.repositories;
 
 import org.sid.ebankingservice.entities.BankAccount;
+import org.sid.ebankingservice.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
-
+    List<BankAccount> findByType(AccountType type);
 }
